da3668e7f44df709a5248315f377b087a716f03e
sch <salim.chami@myschoolstory.net>
2020-05-23 16:53:24 +0200

    adding spring boot

--- web/src/main/java/io/salimchami/jacoco/coverage/web/MavenAggregateApplication.java
+++ web/src/main/java/io/salimchami/jacoco/coverage/web/MavenAggregateApplication.java
@@ -3,7 +3,7 @@ package io.salimchami.jacoco.coverage.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "io.salimchami.jacoco.coverage")
+@SpringBootApplication(scanBasePackages = {"io.salimchami.jacoco.coverage"})
 public class MavenAggregateApplication {
 
     public static void main(String[] args) {
--- web/src/test/java/io/salimchami/jacoco/coverage/web/WebIT.java
+++ web/src/test/java/io/salimchami/jacoco/coverage/web/WebIT.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
