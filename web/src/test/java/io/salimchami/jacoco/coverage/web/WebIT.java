2d75e6f8b22f3c6addd3b35bf8702686b317fb75
sch <salim.chami@myschoolstory.net>
2020-05-23 16:58:31 +0200

    adding spring boot

--- web/src/test/java/io/salimchami/jacoco/coverage/web/WebIT.java
+++ web/src/test/java/io/salimchami/jacoco/coverage/web/WebIT.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,7 +19,7 @@ public class WebIT {
 
     @Test
     public void integrationTest() throws Exception {
-        mockMvc.perform(get("/api/web"))
+        mockMvc.perform(get("/web"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("oh yeah !"));
     }
